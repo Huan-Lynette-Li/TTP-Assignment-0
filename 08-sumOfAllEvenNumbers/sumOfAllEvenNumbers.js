31838c5277ec87e97e3741bf4daabff9ca0e09de
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 11:12:49 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  let count = 0;
+  nums.forEach((number) => {
+    count += (number % 2 === 0) ? 1 : 0;
+  });
+  return count;
 }
 
 // Do not edit this line;
