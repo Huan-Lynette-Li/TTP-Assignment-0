e85344e9e72b9c1b260df6024dc21142e181feab
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 00:00:49 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+   let result = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === true || arr[i] === false || typeof arr[i] === "string") {
+      result++;
+    }
+  }
+  return result;
 }
 
 // Do not edit this line;
