e140fc88e4f3a3f6a049f8e7c4b2340f7da8be92
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 12:13:17 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
   // Insert code here;
+  let res = [];
+    let indexes = [];
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (target === nums[i] + nums[j] && !indexes.includes(i) && !indexes.includes(j)) {
+                res.push([nums[i], nums[j]]);
+                indexes.push(i);
+                indexes.push(j);
+            }
+        }
+    }
+    return res;
 }
 
 // Do not edit this line;
