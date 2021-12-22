f5f31077291b70000a0894aec9e6b62b40df5966
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-21 23:56:08 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let result = [];
+ for(let i = 0; i < arr.length; i++) {
+     if (arr[i] === true || arr[i] === false ) {
+         result.push(arr[i]);
+     }
+ }
+ return result.length;
 }
 
 // Do not edit this line;
