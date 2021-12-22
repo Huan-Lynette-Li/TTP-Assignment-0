59d5acec7a83d55222e59f8e8283ad369d445486
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 11:34:58 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,11 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let count = {};
+  word.split('').forEach(function(character) {
+     count[character] ? count[character]++ : count[character] = 1;
+  });
+  return count;
+  
 }
 
 // Do not edit this line;
