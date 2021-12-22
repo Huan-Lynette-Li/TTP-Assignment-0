28e3654e05bd8ec80cb2d1b12c88886bc3230eb5
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-21 23:42:49 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,8 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  return title.toLowerCase().split(' ').map(function(word) {
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ');
 }
 
 // Do not edit this line;
