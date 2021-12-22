7eb75c68e0e31904f2d45aa9be3896e40cde5c63
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 11:15:23 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,10 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let count = 0;
+  nums.forEach((number) => {
+    count += (number >= start && number <= end) ? 1 : 0;
+  })
+  return count;
 }
 
 // Do not edit this line;
