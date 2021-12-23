be43d2eb8e02dac2e723c085ba8bbc3c6b3be06a
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 20:58:06 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,23 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let set = new Set();
+  let array = []
+
+  // Adds all the sets in the input args to an array
+  for (let i = 0; i < arguments.length; i++){
+    array.push(arguments[i])
+  }
+
+  // Loops through the array and each set element in the array and adds the values of the sets to a new set
+  for (let i = 0; i < array.length; i++){
+    for (var elem of array[i]) { 
+      set.add(elem); 
+    }
+  }
+  
+  // For testing
+  console.log(set)
+  return set;
 }
 
 // Do not edit this line;
