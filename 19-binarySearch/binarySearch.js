08f2fe39535de626ca84500bf16a09368b27f0f8
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 20:56:03 -0500

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,23 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let low = 0, high = nums.length - 1;
+    let found = false;
+    let mid;
+    
+    while (!found && low <= high) {
+      mid = Math.floor((low + high) / 2);
+      
+      if (nums[mid] === target) { 
+        found = true;
+      } else if (nums[mid] > target){ 
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return found;
   }
 }
 
