0411beba6e7e35e70543dddbabe3ed73ba0eadef
Huan (Lynette) Li <85309447+Huan-Lynette-Li@users.noreply.github.com>
2021-12-22 20:46:52 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,9 +1,9 @@
 function pairSum(nums, target) {
   // Insert code here;
   if (nums.length <= 1) throw "Length of array is not more than 1!";
-  for (let a = 0; a < nums.length; a++) {
-    for (let b = 0; b < nums.length; b++){
-      if (a !== b && nums[a] + nums[b] === target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++){
+      if (i !== j && nums[i] + nums[j] === target) {
         return true;
       }
     }
